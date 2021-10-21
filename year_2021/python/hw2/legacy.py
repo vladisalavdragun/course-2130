0bf3dab928043935924da10b34fa95a96be733df
vladisalavdragun <92948165+vladisalavdragun@users.noreply.github.com>
2021-10-21 22:08:02 +0300

    Update legacy.py

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -5,6 +5,13 @@ def t1(number):
     Пример: -5 -> 0
     """
     pass
+    if number % 20 == 0:
+        return number
+    else:
+        if number < 0:
+            return (int(number/20)*20)
+        else:
+            return (int(number/20)*20 + 20)
 
 
 def t2(string):
@@ -13,6 +20,13 @@ def t2(string):
     Пример: `abc abc abc` -> `cba cba cba`
     """
     pass
+    lst = []
+    str_splitted = string.split(' ')
+    for i in str_splitted:
+        lst.append(i[::-1])
+    return (' '.join(lst))
+    
+
 
 
 def t3(dictionary):
@@ -20,6 +34,16 @@ def t3(dictionary):
     На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
     """
     pass
+    str_new = ''
+    k = 0
+    
+    for key, value in dictionary.items():
+        if k == len(dictionary) - 1:
+            str_new = str_new + f'{key}: {value}'
+        else:
+            str_new = str_new + f'{key}: {value}; '
+            k += 1
+    return str_new
 
 
 def t4(string, sub_string):
@@ -27,6 +51,10 @@ def t4(string, sub_string):
     проверить есть ли в строке инвертированная подстрока
     """
     pass
+    if string[::-1].find(sub_string) != -1:
+        return True
+    else:
+        return False
 
 
 def t5(strings):
