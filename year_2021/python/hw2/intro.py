29f6bc14d3c6ae5db4083302db2424fd35f8e90c
vladisalavdragun <92948165+vladisalavdragun@users.noreply.github.com>
2021-10-21 22:11:20 +0300

    Update intro.py

--- year_2021/python/hw2/intro.py
+++ year_2021/python/hw2/intro.py
@@ -15,7 +15,8 @@ def reverse(lst: Optional[List[Any]]):
         [4, 3, 2, 1]
     ```
     """
-    return
+    rev_lst = lst[::-1]
+    return rev_lst
 
 
 def filter_by_indices(lst: Optional[List[Any]], indices: Optional[List[Any]]):
@@ -35,4 +36,23 @@ def filter_by_indices(lst: Optional[List[Any]], indices: Optional[List[Any]]):
         [3, 4]
     ```
     """
-    return
+    x = len(lst)
+    i = 0
+    while i != len(indices):
+        if abs(indices[i]) >= x:
+            del indices[i]
+            i -= 1
+        if indices[i] < 0:
+            indices[i] += x
+        i += 1
+    
+    indices.sort()
+    x = len(lst)
+    
+    k = 0
+    for i in set(indices):
+        if(abs(i) >= x):
+            continue
+        del(lst[i - k])
+        k += 1
+    return lst
