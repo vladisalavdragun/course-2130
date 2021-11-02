8f21615340abacd9f5df7bc2dc84164d593c8c89
vladisalavdragun <92948165+vladisalavdragun@users.noreply.github.com>
2021-11-02 18:15:39 +0300

    Update main.py

--- year_2021/python/hw3/main.py
+++ year_2021/python/hw3/main.py
@@ -8,6 +8,11 @@ class Node:
         self.value = value
         self.next_value = None
 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
     def __delitem__(self, value) -> bool:  # True -> if element was deleted else False
         """
         find item then delete
@@ -15,7 +20,23 @@ class Node:
         returns True if element was deleted successfully
                 False else (if element wasn't found
         """
-        # TODO: homework
+        if self.head is None:
+            return False
+        if self.head.value == value:
+            if self.head.next_value:
+                self.head = self.head.next_value
+            else:
+                self.head = None
+            return True
+        elem = self.head.next_value
+        previous_node = self.head
+        while elem:
+            if elem.value == value:
+                previous_node.next_value = elem.next_value
+                return True
+            previous_node = elem
+            elem = elem.next_value
+        return False
 
         pass
 
@@ -23,14 +44,27 @@ class Node:
         """
         Search for element and return
         """
-        # TODO: homework
+        elem = self.head
+        while elem:
+            if elem.value == value:
+                return elem
+            elem = elem.next_value
+        return elem
         pass
+
+
 
     def append(self, value):
         """
         Add element to linked list
         """
-        # TODO: homework
+        if not self.head:
+            self.head = Node(value)
+            return
+        node = self.head
+        while node.next_value:
+            node = node.next_value
+        node.next_value = Node(value)
         pass
 
 
